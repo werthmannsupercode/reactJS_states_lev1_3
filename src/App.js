d8b1925e768eed995b69b5ac55ddaac23f36bf85
werthmannsupercode <werthmannsupercode@gmail.com>
2022-06-10 12:52:56 +0200

    first try

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [number, setNumber] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>COUNTER</h1>
+      <div className="innerContainer">
+        <button onClick={() => setNumber(number + 1)}>+</button>
+        <p>{number}</p>
+        <button onClick={() => setNumber(number - 1)}>-</button>
+      </div>
+      <button onClick={() => setNumber(number - number)}>reset</button>
     </div>
   );
 }
